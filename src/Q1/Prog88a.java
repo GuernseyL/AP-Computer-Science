a81c6ad4324ecad24762c3bb22e4ae8ef24e4597
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-09-11 11:26:00 -0500

    9/11

--- src/Q1/Prog88a.java
+++ src/Q1/Prog88a.java
@@ -3,37 +3,44 @@ import java.util.*;
 
 public class Prog88a {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter number 1: ");
-        double num1 = input.nextDouble();
+        int num1 = (int) (Math.random() * (13-1)) +1;
+        int num2 = (int) (Math.random() * (20-2)) +2;
+        //Scanner input = new Scanner(System.in);
 
-        System.out.print("Please enter number 2: ");
-        double num2 = input.nextInt();
+        //System.out.print("Please enter number 1: ");
+        //double num1 = input.nextDouble();
 
-        double Sum = num1 + num2;
-        double Difference = num1 - num2;
-        double Product = num1 * num2;
-        double Average = (num1 + num2) / 2;
-        double Absolute = Math.abs(num1 - num2);
-        double Greater = 0.0;
-        double Lesser = 0.0;
+        //System.out.print("Please enter number 2: ");
+        //double num2 = input.nextInt();
+
+        int Sum = num1 + num2;
+        int Difference = num1 - num2;
+        int Product = num1 * num2;
+        int Average = (num1 + num2) / 2;
+        int Absolute = Math.abs(Difference);
+        int max, min;
         if (num1 > num2) {
-            Greater = num1;
-            Lesser = num2;
+            max = num1;
         }
         else {
-            Greater = num2;
-            Lesser = num1;
+            max = num2;
+        }
+        if (num1 == max) {
+            min = num2;
+        }
+        else {
+            min = num1;
         }
 
+        System.out.println("Your originally numbers are " + num1 + " and " + num2);
         System.out.println("Sum = " + Sum);
         System.out.println("Difference  = " + Difference);
         System.out.println("Product = " + Product);
         System.out.println("Average = " + Average);
         System.out.println("Absolute Differance = " + Absolute);
-        System.out.println("Max = " + Greater);
-        System.out.println("Min = " + Lesser);
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
 
     }
 }
