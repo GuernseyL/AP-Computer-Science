620608329dc7b766160db7f1b4be4538a8f9a773
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2025-02-21 11:24:07 -0600

    2/21

---
+++ src/Q3/Prog152aRecursive.java
@@ -0,0 +1,15 @@
+package Q3;
+
+import java.util.Scanner;
+
+public class Prog152aRecursive {
+    public static int SumSeries(int n) {
+        if (n >= 9669) return 9669;         // Base/Ending Case
+        return n + SumSeries(n+3); // Recursive Case
+    }
+
+    public static void main(String[] args) {
+        int fact = SumSeries(3);
+        System.out.println("The sum of the multiples of 3, from 3 to 9669 is: " + fact);
+    }
+}
