a588098a3c7759162ca73d71970dc69f23f57302
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2025-04-04 11:20:17 -0500

    4/4/25

--- src/Q3/Tester.java
+++ src/Q3/Tester.java
@@ -3,15 +3,5 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String cityZip = input.nextLine();
-        String delta = "";
-        for (int x = 0; x < cityZip.length(); x++) {
-            if (cityZip.substring(x, x+1).equals(",")) {
-                x += 999999;
-            }
-            else { delta += cityZip.substring(x, x+1); }
-        }
-        System.out.println(delta);
     }
 }
