49cafeb5b812b17eee2ccbb22e1c2db157b002be
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2025-04-23 11:54:43 -0500

    4/23/2025

---
+++ src/Q4/VirtualPetLab/Fox.java
@@ -0,0 +1,23 @@
+package Q4.VirtualPetLab;
+
+public class Fox extends Pet {
+    public Fox(String name) {
+        super(name);
+        super.setImage(ImageHelper.getRandomCatImg());
+    }
+
+    @Override
+    public void feed() {
+        // TODO: Implement feeding behavior for Fox
+    }
+
+    @Override
+    public void play() {
+        // TODO: Implement playing behavior for Fox
+    }
+
+    @Override
+    public void sleep() {
+        // TODO: Implement sleeping behavior for Fox
+    }
+}
